c99d04fc3f02b38d64ad3ea48f294d1e4e2b4210
lottsy <driver461461@gmail.com>
2022-12-19 23:11:31 +0300

    animations_1

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+new WOW().init();
+
+/* move background with cursor (breaks on mobile and high definition so don't use)*/
+/* const el = document.querySelector(".main");
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.pageX / 50 + "px";
+  el.style.backgroundPositionY = -e.pageY / 50 + "px";
+  console.log(e);
+}); */
